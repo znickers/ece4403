f3cf5926c1c5d5e77a7272cb23c8d6ad00515e54
salamiman <onegoodsandwich@gmail.com>
2020-02-23 16:31:17 -0400

    Added input csv files and updated CSVReader to read from files. Added a test driver in Registrar.

--- SubstituteTeacherTracker/src/CSVReader.java
+++ SubstituteTeacherTracker/src/CSVReader.java
@@ -12,19 +12,25 @@ public class CSVReader {
 	}
 	
 	// TODO: finish reading absences
-	// maybe change return type...
-	public static void readAbsences() {
+	// read absences from absences.csv
+	public static void readAbsences(String filename) {
 		try {
-			csvParser = new CSVParser(new FileReader("absences.csv"), CSVFormat.EXCEL.withFirstRecordAsHeader());
+			csvParser = new CSVParser(new FileReader(filename), CSVFormat.EXCEL.withFirstRecordAsHeader());
 			
-			// TODO: figure out csv structure and parse accordingly
+			// parse thru entries in absences.csv
 			for (CSVRecord record : csvParser) {
-				// example reading from class resources
-				// Change allll of this
 				String name = record.get("name");
-				String phoneNumber = record.get("phone number");
-				String email = record.get("email");
-				System.out.println("CSV Record: " + name + " | " + phoneNumber + " | " + email);
+				String startDate = record.get("start_date");
+				String startPeriod = record.get("start_period");
+				String endDate = record.get("end_date");
+				String endPeriod = record.get("end_period");
+				String location = record.get("location");
+				String teachables = record.get("teachables");
+				
+				// DEBUG CODE: output csvParser results
+				System.out.println("Absence Record: "+name+" | "+startDate+" | "+startPeriod+" | "+endDate+" | "+endPeriod+" | "+location+" | "+teachables);
+				
+				// TODO: create an absence
 			}
 			
 			csvParser.close();
@@ -34,23 +40,79 @@ public class CSVReader {
 		}
 	}
 	
-	public static void readSubstitutes() {
+	// TODO: finish reading absences
+	// read substitutes from substitutes.csv
+	public static void readSubstitutes(String filename) {
 		try {
-			csvParser = new CSVParser(new FileReader("substitutes.csv"), CSVFormat.EXCEL.withFirstRecordAsHeader());
+			csvParser = new CSVParser(new FileReader(filename), CSVFormat.EXCEL.withFirstRecordAsHeader());
 			
-			// TODO: figure out csv structure and parse accordingly
+			// parse thru entries in substitutes.csv
 			for (CSVRecord record : csvParser) {
-				// example reading from class resources
-				// Change allll of this
 				String name = record.get("name");
-				String phoneNumber = record.get("phone number");
-				String email = record.get("email");
-				System.out.println("CSV Record: " + name + " | " + phoneNumber + " | " + email);
+				String startDate = record.get("start_date");
+				String startPeriod = record.get("start_period");
+				String endDate = record.get("end_date");
+				String endPeriod = record.get("end_period");
+				String blacklist = record.get("blacklist");
+				String teachables = record.get("teachables");
+				
+				// DEBUG CODE: output csvParser results
+				System.out.println("Substitute Record: "+name+" | "+startDate+" | "+startPeriod+" | "+endDate+" | "+endPeriod+" | "+blacklist+" | "+teachables);
+				
+				// TODO: create a substitute
 			}
 			
 			csvParser.close();
 		} catch(IOException ioe) {
 			System.out.println("Unable to find substitutes.csv");
+			ioe.printStackTrace();
+		}
+	}
+	
+	// TODO: finish reading preferred subs
+	// read preferred substitutes from preferred.csv
+	public static void readPreferred(String filename) {
+		try {
+			csvParser = new CSVParser(new FileReader(filename), CSVFormat.EXCEL.withFirstRecordAsHeader());
+			
+			// parse thru entries in preferred.csv
+			for (CSVRecord record : csvParser) {
+				String teacherName = record.get("teacher");
+				String subName = record.get("preferred substitute");
+				
+				// DEBUG CODE: output csvParser results
+				System.out.println("Preferred Record: "+teacherName+" | "+subName);
+				
+				// TODO: assign preferred Sub to Teacher
+			}
+			
+			csvParser.close();
+		} catch(IOException ioe) {
+			System.out.println("Unable to find preferred.csv");
+			ioe.printStackTrace();
+		}
+	}
+	
+	// TODO: finish reading on-call contracts
+	// read on-call contracts from oncalls.csv
+	public static void readOnCalls(String filename) {
+		try {
+			csvParser = new CSVParser(new FileReader(filename), CSVFormat.EXCEL.withFirstRecordAsHeader());
+			
+			// parse thru entries in oncalls.csv
+			for (CSVRecord record : csvParser) {
+				String subName = record.get("substitute");
+				String location = record.get("on-call contract location");
+				
+				// DEBUG CODE: output csvParser results
+				System.out.println("On-Call Record: "+subName+" | "+location);
+				
+				// TODO: assign on-call contract to Sub
+			}
+			
+			csvParser.close();
+		} catch(IOException ioe) {
+			System.out.println("Unable to find oncalls.csv");
 			ioe.printStackTrace();
 		}
 	}
--- SubstituteTeacherTracker/src/Registrar.java
+++ SubstituteTeacherTracker/src/Registrar.java
@@ -1,4 +1,13 @@
 
 public class Registrar {
-
+	
+	// DEBUG: test driver
+	public static void main(String[] args) {
+		
+		// DEBUG: test code for CSVReader
+		CSVReader.readAbsences("absences.csv");
+		CSVReader.readSubstitutes("substitutes.csv");
+		CSVReader.readPreferred("preferred.csv");
+		CSVReader.readOnCalls("oncalls.csv");
+	}
 }
