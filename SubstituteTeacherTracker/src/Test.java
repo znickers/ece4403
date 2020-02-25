18a0b7d2810fb81b2f7dc5624e2fd18def84bb17
cmacdo20 <cmacdo20@unb.ca>
2020-02-25 16:36:47 -0400

    Date Converter

---
+++ SubstituteTeacherTracker/src/Test.java
@@ -0,0 +1,22 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Test {
+	
+	public int[] convertDate(int year, int month, int day) {
+		LocalDate date = LocalDate.of(year, month, day);
+	    DateTimeFormatter Weekformatter = DateTimeFormatter.ofPattern("ww"); //ISO Calendar ww - gives ISO week (1 to 52)
+	    String ISOWeek = date.format(Weekformatter);
+	    DateTimeFormatter Dayformatter = DateTimeFormatter.ofPattern("c"); //ISO Calendar 
+	    String ISODay = date.format(Dayformatter);
+	    int[] ISO = new int[2];
+	    
+	    ISO[0] = Integer.parseInt(ISOWeek);
+	    ISO[1] = Integer.parseInt(ISODay);
+	    return ISO;
+	    
+	    //Its treating Sunday as 1 and Saturday as 7, for the weekdays (2-6) the week is correct
+	    //week is also correct for sunday but off by one for staturday. 
+	}
+}
